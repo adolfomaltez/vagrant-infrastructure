d806a40dae6ee6271b205bade661df90c565120b
adolfomaltez <adolfomaltez@gmail.com>
2024-01-22 09:06:57 -0600

    Adding antrea.yaml file to be installed by provision script to get Antrea CNI on kubernetes cluster

--- k3s-alma8/provision-k3s.sh
+++ k3s-alma8/provision-k3s.sh
@@ -1,16 +1,18 @@
 # Install k3s on Alma8 machine
 
-
-# Copy the previous downloaded k3s binaries (airgap-image) to the vagrant box
-
+# Copy all the needed k3s binaries and files (airgap-image) to the vagrant box
 sudo mkdir -p /var/lib/rancher/k3s/agent/images/
+sudo mkdir -p /var/lib/rancher/k3s/server/manifests/
 sudo cp /vagrant/k3s /usr/local/bin/
 sudo cp /vagrant/k3s-airgap-images-amd64.tar /var/lib/rancher/k3s/agent/images/
+sudo cp /vagrant/antrea.yaml /var/lib/rancher/k3s/server/manifests/
 sudo chmod +x /vagrant/install.sh
 sudo chmod +x /usr/local/bin/k3s
 
+## Install k3s single server (default CNI)
+#INSTALL_K3S_SKIP_DOWNLOAD=true /vagrant/install.sh
+#sudo chmod +r /etc/rancher/k3s/k3s.yaml
 
-# Install k3s single server
-
-INSTALL_K3S_SKIP_DOWNLOAD=true /vagrant/install.sh
+## Install k3s single server + Antrea CNI
+INSTALL_K3S_SKIP_DOWNLOAD=true /vagrant/install.sh --flannel-backend=none --disable-network-policy
 sudo chmod +r /etc/rancher/k3s/k3s.yaml

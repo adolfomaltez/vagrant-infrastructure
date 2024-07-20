4d7fd0e6c00e3e467b0123efc3dd7191246875de
adolfomaltez <adolfomaltez@gmail.com>
2024-07-19 22:19:20 -0500

    Downgrading kind version from v0.23.0 to v0.20.0 because rancher v2.8.5 chart requires kubeVersion: < 1.29.0-0

--- rancher-debian12/provision.sh
+++ rancher-debian12/provision.sh
@@ -23,7 +23,7 @@ su vagrant
 
 
 ## Install kind
-curl -sLo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
+curl -sLo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
